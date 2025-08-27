658ae039fe0f1ad20036862655f9a0eebc43a943
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-08-27 16:50:22 -0400

    documentation

--- src/engine/net/udp.h
+++ src/engine/net/udp.h
@@ -37,6 +37,10 @@ public:
     static constexpr size_t MAX_SAFE_PAYLOAD_SIZE = 508;
     static constexpr size_t MAX_IPV4_UDP_SIZE = 65507;
 
+    /// @brief Creates a new UDP socket, allocating any necessary memory.
+    /// @return The new UDP socket.
+    ///
+    /// https://man7.org/linux/man-pages/man2/socket.2.html
     static UdpSocket create();
 
     UdpSocket(const UdpSocket&) = delete;
@@ -50,15 +54,39 @@ public:
     ~UdpSocket() noexcept;
 
     #if defined(_WIN32)
+    /// @return Get the socket as the raw socket handle for win32.
     operator SOCKET() const;
     #elif defined(__GNUC__) || defined(__clang__)
+    /// @return Get the socket as the raw socket file descriptor for posix.
     operator int() const { return this->socket_; }
     #endif
 
+    /// @brief Checks if this socket has some data to read from without
+    /// blocking. Blocks execution on this thread until either the timeout 
+    /// expires, or the socket status is ready.
+    /// @param timeoutMicroseconds How long to block in microseconds.
+    /// @return `true` if there is some data to read, `false` otherwise.
+    ///
+    /// https://man7.org/linux/man-pages/man2/select.2.html
     bool readable(long timeoutMicroseconds = 5) const;
 
+    /// @brief Checks if this socket can be written to without 
+    /// blocking. Blocks execution on this thread until either the timeout
+    ///  expires, or the socket status is ready.
+    /// @param timeoutMicroseconds How long to block in microseconds.
+    /// @return `true` if writing can be done without blocking, `false` 
+    /// otherwise.
+    ///
+    /// https://man7.org/linux/man-pages/man2/select.2.html
     bool writeable(long timeoutMicroseconds = 5) const;
 
+    /// @brief Binds an address and port to the socket. It is required to
+    /// assign a local address to the socket in order for it to receive
+    /// connections.
+    /// @param addr The address and port to bind to.
+    /// @return Nothing on success, or a string indicating an error message.
+    ///
+    /// https://man7.org/linux/man-pages/man2/bind.2.html
     std::expected<void, std::string> bind(const UdpTransportAddress& addr);
 
     class ReceiveBytes {
@@ -80,8 +108,32 @@ public:
         ReceiveBytes& operator=(ReceiveBytes&&) noexcept = delete;
     };
 
+    /// @brief Reads a packet in the form of bytes from the socket. This is a
+    /// blocking operation. Use with `UdpSocket::readable()` to make
+    /// non-blocking IO.
+    /// @return Either the bytes and the received ipv4 address and port, or
+    /// a string indicating an error message.
+    ///
+    /// https://linux.die.net/man/2/recvfrom
+    ///
+    /// # Fatal Error
+    ///
+    /// Only one thread may be receiving from `this` socket at a time.
+    /// Should two threads try to receive from the same socket at the same
+    /// time, the program will terminate.
     std::expected<ReceiveBytes, std::string> receiveFrom();
 
+    /// @brief Writes a packet in the form of bytes to the socket. This is a
+    /// blocking operation. Use with `UdpSocket::writeable()` to make
+    /// non-blocking IO.
+    /// @param bytes The bytes to write into the socket. Does not need to be 0 
+    /// terminated.
+    /// @param len The amount of bytes to write. See 
+    /// `UdpSocket::MAX_SAFE_PAYLOAD_SIZE` and `UdpSocket::MAX_IPV4_UDP_SIZE`.
+    /// @param to The destination address and port to send to.
+    /// @return Nothing on success, or a string indicating an error message.
+    ///
+    /// https://linux.die.net/man/2/sendto
     std::expected<void, std::string> sendTo(const uint8_t* bytes, uint16_t len, const UdpTransportAddress& to);
 
 private:
